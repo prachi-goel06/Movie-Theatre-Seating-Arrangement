5ceb6019b96480b4ea3c0b16523ea705f32ff82c
Prachi Goel <prachi.goel06@gmail.com>
2018-09-22 02:05:52 -0500

    mistake in summation

--- inputFileMaker.py
+++ inputFileMaker.py
@@ -11,9 +11,9 @@ import random
 x=0
 inputFile=open("InputFile.txt","w+")
 i=1
-while x<1000:
+while x<300:
      seatsReserved=random.randint(1,20)
-     x=+seatsReserved
+     x+=seatsReserved
      inputFile.write('{} {}{}'.format("R"+'{:04d}'.format(i),seatsReserved,'\n'))
      i=i+1
 
